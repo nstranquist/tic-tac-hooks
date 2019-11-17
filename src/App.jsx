09a73638899c129ddacc9b24b618c3e6c31ed0f8
Nico Stranquist <nstranquist@gmail.com>
2019-11-17 17:26:02 -0600

    cleanup code a bit, next will implement winner checking

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ import './app.css'
 // ]
 
 const App = () => {
-  const [hasWon, setHasWon] = useState(false)
   const [winner, setWinner] = useState(null)  // active state will be string or boolean
   const [board, setBoard] = useState([[],[],[]])  // will want to instantiate and map
   const [p1Turn, setP1Turn] = useState(true)  // check if its' player1's move (true) or player2's (false)
@@ -18,20 +17,37 @@ const App = () => {
     resetBoard()
   }, [])
 
-  // TODO: how to minimize renders?
+  useEffect(() => {
+    // check for winner when board changes
+    console.log("player moved")
+    checkWinner()
+  }, [p1Turn])
+
+  const checkWinner = () => {
+    // check board for 3 in a row
+    // 1. if 3 in a row horizontal (3 possibles)
+    // 2. if 3 in a row vertical (3 possibles)
+    // 3. if 3 in a row diagonal (2 possibles)
+    // if a case does exist, check value and match to winner, setWinner({player})
+  }
 
   const resetBoard = () => setBoard([
     [null,null,null],
     [null,null,null],
     [null,null,null],
   ])
+  const resetGame = () => {
+    resetBoard()
+    setP1Turn(true)
+    setWinner(false)
+  }
 
   const handleTileClick = (rowIndex, colIndex) => {
     console.log('tile clicked in row:', rowIndex, 'and col:', colIndex)
     let tile = board[rowIndex][colIndex]
     let newBoard = board
-    // check if already filled
-    if(tile===null) {
+    // check if already filled or if game is over
+    if(!winner && !tile) {
       // can click tile, then set board to show it's filled (checking p1Turn)
       console.log('setting tile to', p1Turn ? 'p1' : 'p2')
       if(p1Turn)
@@ -43,13 +59,12 @@ const App = () => {
     }
   }
   
-  // fill 3x3 board with tiles, set objects to empty
-
+  // TODO: how to minimize renders?
   return (
     <div className="board-container">
-      <h1 style={{fontWeight:400,fontSize:'22px',padding:'10px',margin:0,textDecoration:'underline',textAlign:'center'}}>
+      <h1 className="board-title">
         Tic Tac Hooks</h1>
-      {hasWon && <p>Winner: {winner}</p> }
+      {winner && <p>Winner: {winner}</p> }
       <div className="board">
         {/* 9 tiles, will want to map */}
         {board.map((row, rowIndex) => {
@@ -64,6 +79,7 @@ const App = () => {
           )
         })}
       </div>
+      {winner && <button onClick={resetGame}>Restart</button>}
     </div>
   );
 }
